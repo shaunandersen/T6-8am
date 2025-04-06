0ac6844f4a49298116cc40efd24494109ed683a3
JoshKhan6 <s4885415@student.uq.edu.au>
2025-04-06 23:28:22 +0000

    Complete

--- check_positive.py
+++ check_positive.py
@@ -6,6 +6,6 @@ def is_positive(number):
     return False
 
 # Testing
-test_number = -5
-output = is_positive(test_number)
+number = -5
+output = is_positive(number)
 print("Is the number positive?", output)
